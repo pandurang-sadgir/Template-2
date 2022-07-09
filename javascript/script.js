4f75c4b414b83cf227f1fb95b34378d30cfd938d
pandurang sadgir <psadgir111@gmail.com>
2022-07-09 19:23:35 +0530

    navbar responsiveness

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
     $(window).scroll(function () {
 
         if(this.scrollY>20){
+            $('header').addClass('addClass');
+        }else{
+            $('header').removeClass('addClass');
+
         }
         
+    });
+
+    $('header .navbar .menu-btn').click(function(){
+
+        $('header .navbar .menu').toggleClass('active');
     });
 })
