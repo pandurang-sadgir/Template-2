a79e4c5f2fbd6f9e6a7df84d2a6099ab36928096
pandurang sadgir <psadgir111@gmail.com>
2022-07-10 10:24:25 +0530

    scrollUp button add

--- javascript/script.js
+++ javascript/script.js
@@ -3,11 +3,27 @@ $(document).ready(function(){
 
         if(this.scrollY>20){
             $('header').addClass('addClass');
+
+
         }else{
             $('header').removeClass('addClass');
 
         }
+
+        if(this.scrollY>500){
+            $('.scrollUpBtn').addClass('scrollActive');
+
+        }else{
+            $('.scrollUpBtn').removeClass('scrollActive');
+
+        }
         
+    });
+
+    $('.scrollUpBtn').click(function(){
+        // $(window).scrollTop(0);
+        $('html').animate({scrollTop:0});
+
     });
 
     $('header .navbar .menu-btn,header .navbar .menu a').click(function(){
